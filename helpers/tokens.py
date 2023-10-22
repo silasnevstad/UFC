af0ecc722b7cc7eb2debc77e5d66c83bf6488f1b
Silas Nevstad <nevstads@gmail.com>
2023-10-22 17:52:14 -0400

    Day 2 (Hour 3)

--- helpers/tokens.py
+++
@@ -1,7 +0,0 @@
-import tiktoken
-
-enc = tiktoken.get_encoding("cl100k_base")
-
-def count_tokens(text):
-    tokens = enc.encode(text)
-    return len(tokens)
--- sources/api/api_client.py
+++
@@ -1,9 +0,0 @@
-class APIClient:
-    def search(self, query):
-        raise NotImplementedError
-
-    def retrieve_item(self, item_id):
-       raise NotImplementedError
-
-    def get_metadata(self, item_id):
-        raise NotImplementedError
